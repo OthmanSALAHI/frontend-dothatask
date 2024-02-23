89f512e9b4ca06d6100400f5bf708bc65feec6f9
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-23 21:21:36 +0100

    modifie something else again

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Signin from "./components/Auth/Signin";
-// import Signup from "./components/Auth/Signup";
+import Signup from "./components/Auth/Signup";
 
 export const App = () => {
   return (
     <div>
-      <Signin />
+      <Signup />
     </div>
   );
 };
