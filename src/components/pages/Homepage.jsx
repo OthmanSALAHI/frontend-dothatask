48d0f2f2be652b80c96641cec0ac998241f53a7b
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-24 19:52:51 +0100

    Refactor homepage design and styles

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Homepage() {
     return (
-        <div className='w-full relative bg-neutral-100'>
-            <div className="md:size-[344px] size-[140px] circle-design buttom-10 mt-[-100px] ml-[-111px] z-[-1]" />
-            <div className="md:size-[268px] size-[95px] circle-design right-[-140px]" />
-            <div className="md:size-[350px] size-[150px] circle-design top-[122px] right-[-140px]" />
-            <div className="text-neutral-800 text-5xl w-[500px] absolute left-[30%] text-center mt-[60px] py-9 font-bold font-['Ubuntu']">Find The Right Tasker</div>
+        <div className='md:w-full relative bg-neutral-100'>
+            <div className="md:size-[314px] size-[140px] circle-design buttom-10 md:mt-[-70px] mt-[-50px] md:ml-[-111px] ml-[-50px] z-[-1]" />
+            <div className="md:size-[238px] size-[95px]  circle-design md:right-[-140px] right-[-40px]" />
+            <div className="md:size-[320px] size-[150px] circle-design md:top-[122px] top-[50px] md:right-[-140px] right-[-40px]" />
+            <div className="text-neutral-800 lg:text-5xl md:text-3xl text-xl absolute md:left-[32%] left-[30%] text-center md:mt-[60px] py-9 font-bold">Find The Right Tasker</div>
         </div>
     )
 }
