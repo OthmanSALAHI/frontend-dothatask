10d8b90dd4b41b24a8927a5081cbe289ebc32b3c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-24 19:54:25 +0100

    Update text alignment in Homepage component

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -6,7 +6,7 @@ function Homepage() {
             <div className="md:size-[314px] size-[140px] circle-design buttom-10 md:mt-[-70px] mt-[-50px] md:ml-[-111px] ml-[-50px] z-[-1]" />
             <div className="md:size-[238px] size-[95px]  circle-design md:right-[-140px] right-[-40px]" />
             <div className="md:size-[320px] size-[150px] circle-design md:top-[122px] top-[50px] md:right-[-140px] right-[-40px]" />
-            <div className="text-neutral-800 lg:text-5xl md:text-3xl text-xl absolute md:left-[32%] left-[30%] text-center md:mt-[60px] py-9 font-bold">Find The Right Tasker</div>
+            <div className="text-neutral-800 lg:text-5xl md:text-3xl text-xl absolute md:left-[32%] left-[28%] text-center md:mt-[60px] py-9 font-bold">Find The Right Tasker</div>
         </div>
     )
 }
