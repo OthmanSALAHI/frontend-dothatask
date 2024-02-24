4592ee2833a5788c9b45a6fba1f93b6d184045c9
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-24 20:44:23 +0100

    Update homepage styling and input font size

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Homepage() {
     return (
         <div className='md:w-full relative bg-neutral-100 z-[-1]'>
-            <div className='block w-full h-[45vh]'>
+            <div className='block w-full md:h-[45vh] h-[30vh]'>
                 <div className="md:size-[314px] size-[140px] circle-design buttom-10 md:mt-[-70px] mt-[-50px] md:ml-[-111px] ml-[-50px] z-[-1]" />
                 <div className="md:size-[238px] size-[95px]  circle-design md:right-[-140px] right-[-50px]" />
                 <div className="md:size-[320px] size-[150px] circle-design md:top-[122px] top-[50px] md:right-[-140px] right-[-50px]" />
@@ -13,7 +13,7 @@ function Homepage() {
                 <div className="flex justify-center items-center">
                     <input type='text' placeholder='Place' className=" place-input rounded-tl-[40px] rounded-bl-[40px]" />
                     <input type='text' placeholder='categorie' className="place-input" />
-                    <button className=" md:text-2xl sm:text-lg text-md font-light bg-emerald-600 h-16 text-white rounded-tr-[40px] md:w-[195px] w-[120px] rounded-br-[40px]"> Search </button>
+                    <button className=" md:text-2xl sm:text-lg text-sm font-light bg-emerald-600 h-16 text-white rounded-tr-[40px] md:w-[195px] w-[120px] rounded-br-[40px]"> Search </button>
                 </div>
             </div>
         </div>
