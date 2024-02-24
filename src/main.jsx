56f7b4b885136769c6e90cbdeeec2b03c9a05e8e
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-24 17:40:15 +0100

    Add Routers component to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import Routers from "./components/routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App/>
+      <Routers/>
     </Router>
   </React.StrictMode>
 );
