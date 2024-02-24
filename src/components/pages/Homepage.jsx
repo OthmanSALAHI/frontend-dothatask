286d401b957aa06f54c87b1ba9183a766f2c50f0
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-24 19:42:10 +0100

    Add Homepage component and update Navbar styling

---
+++ src/components/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Homepage() {
+    return (
+        <div className='w-full relative bg-neutral-100'>
+            <div className="md:size-[344px] size-[140px] circle-design buttom-10 mt-[-100px] ml-[-111px] z-[-1]" />
+            <div className="md:size-[268px] size-[95px] circle-design right-[-140px]" />
+            <div className="md:size-[350px] size-[150px] circle-design top-[122px] right-[-140px]" />
+            <div className="text-neutral-800 text-5xl w-[500px] absolute left-[30%] text-center mt-[60px] py-9 font-bold font-['Ubuntu']">Find The Right Tasker</div>
+        </div>
+    )
+}
+
+export default Homepage
