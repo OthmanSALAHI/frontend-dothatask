1189ac4bc499d4e48f8bc69fcd868ba139eec256
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-27 15:58:08 +0100

    Add Homepage route to Routes.jsx

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -4,10 +4,12 @@ import Signin from '../Auth/Signin';
 import Signup from '../Auth/Signup';
 import ForgotPassword from '../Auth/ForgotPassword';
 import Services from '../pages/Services';
+import Homepage from '../pages/Homepage';
 
 const Routers = () => {
   return (
     <Routes>
+      <Route path="/" element={<Homepage />} />
       <Route path="/services" element={<Services />} />
       <Route path="/login" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
