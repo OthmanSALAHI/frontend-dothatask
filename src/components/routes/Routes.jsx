8aa0499f41e1c5846ebe80a921b24473be4b993c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-27 15:53:27 +0100

    Add Services route to Routes.jsx

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import Signin from '../Auth/Signin';
 import Signup from '../Auth/Signup';
 import ForgotPassword from '../Auth/ForgotPassword';
+import Services from '../pages/Services';
 
 const Routers = () => {
   return (
     <Routes>
-        <Route path="/login" element={<Signin/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/forgotpassword" element={<ForgotPassword/>} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/login" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
     </Routes>
   );
 };
