1e5f3efb1a871dd78bf8cc90d61091b24575cda6
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-29 14:16:52 +0100

    Refactor ServiceDetails component layout and styling

--- src/components/pages/ServiceDetails.jsx
+++ src/components/pages/ServiceDetails.jsx
@@ -4,13 +4,13 @@ import ServiceProfile from './ServiceProfile';
 
 function ServiceDetails() {
   return (
-    <div className='relative bg-neutral-100 lg:m-[100px] md:m-[60px] '>
-      <div className='w-full flex lg:flex-row flex-col items-center justify-center lg:gap-[50px] gap-[2px] m-[20px]'>
+    <div className='relative bg-neutral-100 lg:m-[100px] md:m-[60px] flex flex-col justify-center items-center'>
+      <div className='w-full flex items-center justify-center lg:gap-[50px] gap-[2px] bg-black lg:m-[20px]'>
         <SliderImages />
           <ServiceProfile/>
       </div>
-        <div className="w-[1330px] h-auto p-9 m-[20px] bg-white shadow flex-col justify-start items-start gap-10 inline-flex">
-            <div className="text-neutral-800 text-2xl font-bold font-Ubuntu">About This Advertisement</div>
+        <div className="w-[1330px] h-auto p-9 m-[20px] bg-white shadow gap-10">
+            <div className="text-neutral-800 text-2xl font-bold font-Ubuntu my-5">About This Advertisement</div>
             <div className="self-stretch w-[1050px]">
               <span className="text-neutral-800 text-xl font-normal font-Ubuntu">Welcome to my Modern Minimalist Luxury Logo Design Gig. <br/> <br/> Are you looking for a Minimalist logo design? Look no further! Why nowadays we see mostly Minimal logos for successful brands like Instagram, Twitter & many more? Because it’s an Era of the clean and minimal theme.<br/> <br/> WHY ME:<br/> <br/>
               <ul className='list-disc ml-8'>
