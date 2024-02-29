36e6dc19b0f499d21caa0f906727bd6bcd6b0716
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-29 14:10:29 +0100

    Refactor ServiceDetails component layout

--- src/components/pages/ServiceDetails.jsx
+++ src/components/pages/ServiceDetails.jsx
@@ -4,9 +4,32 @@ import ServiceProfile from './ServiceProfile';
 
 function ServiceDetails() {
   return (
-    <div className='w-full flex lg:flex-row flex-col items-start relative bg-neutral-100 lg:m-[100px] md:m-[60px] lg:gap-[50px] gap-[2px] m-[20px]'>
-       <SliderImages />
-        <ServiceProfile/>
+    <div className='relative bg-neutral-100 lg:m-[100px] md:m-[60px] '>
+      <div className='w-full flex lg:flex-row flex-col items-center justify-center lg:gap-[50px] gap-[2px] m-[20px]'>
+        <SliderImages />
+          <ServiceProfile/>
+      </div>
+        <div className="w-[1330px] h-auto p-9 m-[20px] bg-white shadow flex-col justify-start items-start gap-10 inline-flex">
+            <div className="text-neutral-800 text-2xl font-bold font-Ubuntu">About This Advertisement</div>
+            <div className="self-stretch w-[1050px]">
+              <span className="text-neutral-800 text-xl font-normal font-Ubuntu">Welcome to my Modern Minimalist Luxury Logo Design Gig. <br/> <br/> Are you looking for a Minimalist logo design? Look no further! Why nowadays we see mostly Minimal logos for successful brands like Instagram, Twitter & many more? Because it’s an Era of the clean and minimal theme.<br/> <br/> WHY ME:<br/> <br/>
+              <ul className='list-disc ml-8'>
+                <li>Talented and Dedicated Designer</li>
+                <li>Fully custom made, creative, original, UNIQUE and AWESOME designs</li>
+                <li>48 to 72 hrs super fast delivery</li>
+                <li>Professional customer support 24/7</li>
+                <li>High Quality work 100% money back policy if not satisfied</li>
+                <li>Swift Communication</li>
+                <li>Money-Back Guarantee</li>
+                <li>Unlimited Revisions Depending upon the Package</li>
+              </ul>
+              <br/>
+              <br/>
+              Minimalist Logo | Minimal | Professional | Modern | Text | Vintage | Badge | Hand drawn | Feminine | Signature | Logo Design | Monogram | Real Estate Logo | Luxury 
+              <br/>
+              <br/>
+              Order right now or you will regret by not getting your logo designed by the most amazing and professional designer :)</span></div>
+        </div>
     </div>
   )
 }
