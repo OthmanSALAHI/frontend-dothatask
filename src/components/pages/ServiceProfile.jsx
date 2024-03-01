68b8c1aae4b03f39397023172eac7a7abf21e2e3
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-01 16:26:41 +0100

    Update ServiceProfile component styles

--- src/components/pages/ServiceProfile.jsx
+++ src/components/pages/ServiceProfile.jsx
@@ -8,14 +8,14 @@ function ServiceProfile() {
     const rating = 4
     const remain = 5 - rating
   return (
-    <div className="md:w-[390px] bg-white md:h-[495px] w-[150px] h-[150px] pt-4 p-[30px] mt-[120px] pl-2 shadow flex-col justify-start items-start md:gap-[57px] gap-[10px] inline-flex">
+    <div className="md:w-[390px] lg:bg-white md:h-[495px] w-[280px] h-[200px] pt-4 p-[30px] ml-5  lg:mt-[120px] mt-8 pl-2 shadow flex-col justify-start items-start md:gap-[57px] gap-[10px].">
     <div className="justify-start items-center md:gap-[17px] gap-[2px] inline-flex">
         <img className="md:size-[50px] size-[15px] object-cover rounded-full" src={worker} />
         <div className="text-center text-stone-950 md:text-xl text-xs font-medium font-Ubuntu">Alexander Elijah</div>
     </div>
     <div className="flex-col justify-start items-start md:gap-9 flex w-[150px] h-[90px]">
-        <div className="justify-start items-end md:gap-2.5 gap-1 inline-flex">
-            <div className="text-center text-stone-950 md:text-2xl text-xxs font-normal font-Ubuntu">Ratings:</div>
+        <div className="justify-start items-end gap-2.5 inline-flex">
+            <div className="text-center text-stone-950 md:text-2xl text-xm font-normal font-Ubuntu">Ratings:</div>
             <div className="justify-start items-center gap-1 flex">
                 <div className="justify-start items-start flex">
                     {Array(rating).fill().map((_, index) => (
@@ -26,13 +26,13 @@ function ServiceProfile() {
                         <FaRegStar key={index} className="md:size-[30px] size-[8px] relative text-do_green" />
                         )}
                 </div>
-                <div className="text-center text-neutral-600 md:text-2xl text-xxs font-normal font-Ubuntu">(11)</div>
+                <div className="text-center text-neutral-600 md:text-2xl text-xm font-normal font-Ubuntu">(11)</div>
             </div>
         </div>
-        <div className="text-center"><span className="text-stone-950 md:text-2xl text-xxs font-normal font-Ubuntu">Orders: </span><span className="text-stone-950 md:text-2xl text-xxs font-light font-Ubuntu">33</span></div>
-        <div className="text-center flex gap-[2px]"><span className="text-stone-950 md:text-2xl text-xxs font-normal font-Ubuntu">From: </span><span className="text-stone-950 md:text-2xl text-xxs font-light font-Ubuntu">Casablanca</span></div>
-        <div className="text-left flex gap-[2px]"><span className="text-stone-950 md:w-[170px] w-auto md:text-2xl text-xxs font-normal font-Ubuntu">Phone Number: </span><span className="text-stone-950 md:text-2xl text-xxs font-light font-Ubuntu">0611214110</span></div>
-        <div className="text-left flex gap-[2px]"><span className="text-stone-950 md:w-[140px] w-auto md:text-2xl text-xxs font-normal font-Ubuntu">Service Price: </span><span className="text-stone-950 md:text-2xl text-xxs font-light font-Ubuntu">500DH</span></div>
+        <div className="text-center"><span className="text-stone-950 md:text-2xl text-xm font-normal font-Ubuntu">Orders: </span><span className="text-stone-950 md:text-2xl text-xm font-light font-Ubuntu">33</span></div>
+        <div className="text-center flex gap-[2px]"><span className="text-stone-950 md:text-2xl text-xm font-normal font-Ubuntu">From: </span><span className="text-stone-950 md:text-2xl text-xm font-light font-Ubuntu">Casablanca</span></div>
+        <div className="text-left flex gap-4"><span className="text-stone-950 md:w-[250px] w-auto md:text-2xl text-xm font-normal font-Ubuntu">Phone Number: </span><span className="text-stone-950 md:text-2xl text-xm font-light font-Ubuntu">0611214110</span></div>
+        <div className="text-left flex gap-[2px]"><span className="text-stone-950 md:w-[140px] w-auto md:text-2xl text-xm font-normal font-Ubuntu">Service Price: </span><span className="text-stone-950 md:text-2xl text-xm font-light font-Ubuntu">500DH</span></div>
     </div>
 </div>
   )
