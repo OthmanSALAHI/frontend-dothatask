ec83159dab79c5a0d86247868f993a458e7bc070
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-01 16:38:00 +0100

    Update SliderImages component styles

--- src/components/pages/SliderImages.jsx
+++ src/components/pages/SliderImages.jsx
@@ -28,13 +28,13 @@ function SliderImages() {
     };
   return (
     <div className='md:w-auto w-[480px] ml-3'>
-        <div className="text-neutral-800 md:text-4xl text-md font-bold font-Ubuntu">Design a logo for your company or organization</div>
+        <div className="text-neutral-800 lg:w-auto w-[250px] md:text-4xl text-md font-bold font-Ubuntu">Design a logo for your company or organization</div>
             <div className='my-5'>
                 <span className="text-neutral-800 md:text-xl text-sm font-medium font-Ubuntu">Category:</span>
                 <span className="text-neutral-800 md:text-xl text-sm font-normal font-Ubuntu"> Logo Designer</span>
             </div>
-            <div className="grid md:gap-2 grid-col-5 md:w-[910px] w-[390px]" style={{width: '', height: ''}}>
-                <img className="md:w-[910px] md:h-[396px] w-[270px] h-auto object-cover col-span-5" src={selected} />
+            <div className="grid md:gap-2 grid-col-5 md:w-[910px] w-[290px]" style={{width: '', height: ''}}>
+                <img className="md:w-[910px] md:h-[396px] w-[290px] h-auto object-cover col-span-5" src={selected} />
                 <div className='flex'>
                 {images.map((image, index) => (
                     <img className="images-service" style={{opacity: image === selected ? 1 : 0.5 }} key={index} onClick={handleClick} name={image} src={image} />
