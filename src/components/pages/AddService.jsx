94ddabccfc12aa2e74fa5d58bd268cd33e052425
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-02 11:29:46 +0100

    Refactor AddService component to center the heading

--- src/components/pages/AddService.jsx
+++ src/components/pages/AddService.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function AddService() {
   return (
-    <div>
-        
+    <div className='relative'>
+        <div className='flex justify-center items-center h-[100vh]'>
+            <h1 className='text-4xl font-bold text-center'>Add Service</h1>
+        </div>
     </div>
   )
 }
