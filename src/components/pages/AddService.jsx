9a5cfaa3ab1d8f490ce47f171e98a8c6a7a85c76
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-02 12:44:43 +0100

    Update input and select styles in AddService component

--- src/components/pages/AddService.jsx
+++ src/components/pages/AddService.jsx
@@ -27,15 +27,15 @@ function AddService() {
             <div className="text-neutral-800 text-xl font-medium ">Give your service a title</div>
             <div className="text-neutral-500 text-lg font-normal ">Keep itshort and simple this will help us match you to the right category</div>
             <form className='relative flex flex-col gap-8' action="">
-                <input className=" w-[580px] h-8 p-6 text-neutral-400 text-lg font-normal " placeholder='Exemple: create a professionalbusiness logo design' />
+                <input className=" w-[580px] h-8 p-6 text-do_gray_text_input text-xl font-normal " placeholder='Exemple: create a professionalbusiness logo design' />
                 <div className="text-neutral-800 text-xl font-medium">Category</div>
-                <select {...register('categorie', { required: true })}  className=" w-[580px] h-14 text-xl p-2 font-normal">
+                <select {...register('categorie', { required: true })}  className=" w-[580px] h-14 text-xl p-2 font-normal outline-none text-do_gray_text_input">
                     <option value="">Select category</option>
                     {categories.map((e) => <option value={e}>{e}</option>)}
                 </select>
                 <div className="text-neutral-800 text-xl font-medium ">Service description</div>
                 <div className="w-[580px] text-neutral-500 text-lg font-normal ">enterthe service description accurately that includes all information and conditions</div>
-                <textarea type="comment" placeholder='i need ...' className='w-[580px] h-48 p-2' />
+                <textarea type="comment" placeholder='i need ...' className='w-[580px] h-48 p-2 outline-none' />
                 <label for="zone-file" className="w-[162px] h-12 p-2.5 bg-zinc-200 rounded-lg justify-center items-center gap-2.5 flex">
                     <div className="text-neutral-700 text-base font-normal ">Attach a picture</div>
                     <ImAttachment className="w-4 h-4 opacity-50 relative" />
@@ -43,6 +43,8 @@ function AddService() {
                 </label>
                 <div className="w-[580px] text-neutral-500 text-lg font-normal">Add well-designed pictures to show you service professionally and increase your sales.</div>
                 <div className="text-neutral-800 text-xl font-medium">Service Price</div>
+                <input type='number' placeholder="Exemple: 300DH" className="w-[294px] h-12 p-2.5 bg-white rounded-lg shadow text-do_gray_text_input text-xl font-normal"/>
+
             </form>
         </div>
     </div>
