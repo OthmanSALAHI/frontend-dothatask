957d6e341b8ed1d3da2828721808d12a36f6bf8f
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-03 14:23:21 +0100

    Update input and textarea widths in FormAddService component

--- src/components/AddService/FormAddService.jsx
+++ src/components/AddService/FormAddService.jsx
@@ -31,21 +31,21 @@ function FormAddService() {
             <div className="text-neutral-800 md:text-2xl text-md font-medium ">Give your service a title</div>
             <div className="text-do_gray_text text-lg font-normal ">Keep it short and simple this will help us match you to the right category</div>
             <form className='relative flex flex-col gap-8' onSubmit={handleSubmit(onSubmit)}>
-                <input className=" md:w-[580px] w-[200px] h-8 p-6 text-do_gray_text_input text-xl font-normal " placeholder='Exemple: create a professionalbusiness logo design' />
+                <input className=" md:w-[580px] w-[280px] h-8 p-6 text-do_gray_text_input text-xl font-normal " placeholder='Exemple: create a professionalbusiness logo design' />
                 <div className="text-neutral-800 md:text-2xl text-md mt-5 font-medium">Category</div>
-                <select {...register('categorie', { required: true })}  className=" md:w-[580px] w-[200px] h-14 text-xl p-2 font-normal outline-none text-do_gray_text_input">
+                <select {...register('categorie', { required: true })}  className=" md:w-[580px] w-[280px] h-14 text-xl p-2 font-normal outline-none text-do_gray_text_input">
                     <option value="">Select category</option>
                     {categories.map((e, idx) => <option key={idx} value={e}>{e}</option>)}
                 </select>
                 <div className="text-neutral-800 md:text-2xl text-md mt-5 font-medium ">Service description</div>
-                <div className="md:w-[580px] w-[200px] text-neutral-500 text-lg font-normal ">enterthe service description accurately that includes all information and conditions</div>
-                <textarea type="comment" {...register('description', {required: true})} placeholder='i need ...' className='md:w-[580px] w-[200px] h-48 p-2 outline-none' />
+                <div className="md:w-[580px] w-[280px] text-neutral-500 text-lg font-normal ">enterthe service description accurately that includes all information and conditions</div>
+                <textarea type="comment" {...register('description', {required: true})} placeholder='i need ...' className='md:w-[580px] w-[280px] h-48 p-2 outline-none' />
                 <label htmlFor="zone-file" className="w-[162px] h-12 p-2.5 bg-zinc-200 rounded-lg justify-center items-center gap-2.5 flex">
                     <div className="text-neutral-700 text-base font-normal ">Attach a picture</div>
                     <ImAttachment className="w-4 h-4 opacity-50 relative" />
                     <input id="zone-file" {...register('files', {required: true})} className="hidden w-[162px] h-12 " type="file"/>
                 </label>
-                <div className="md:w-[580px] w-[200px] text-neutral-500 text-lg font-normal">Add well-designed pictures to show you service professionally and increase your sales.</div>
+                <div className="md:w-[580px] w-[200p8] text-neutral-500 text-lg font-normal">Add well-designed pictures to show you service professionally and increase your sales.</div>
                 <div className="text-neutral-800 md:text-2xl text-md mt-5 font-medium">Service Price</div>
                 <input type='number' {...register('price', {required : true})} placeholder="Exemple: 300DH" className="md:w-[294px] w-[130px] h-12 p-2.5 bg-white rounded-lg shadow text-do_gray_text_input text-xl font-normal"/>
                 <div className="text-neutral-800 md:text-2xl text-md mt-5 font-medium">Keywords</div>
