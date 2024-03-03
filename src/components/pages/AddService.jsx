08e6daabc9821ee12d361b6c00f419964791d9c8
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-03 14:23:25 +0100

    Add AddServiceHelp component to AddService page

--- src/components/pages/AddService.jsx
+++ src/components/pages/AddService.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import FormAddService from '../AddService/FormAddService'
+import AddServiceHelp from '../AddService/AddServiceHelp'
 
 
 function AddService() {
     return (
-        <FormAddService />
-        
+        <div className='flex lg:flex-row flex-col gap-5 font-ubuntu'>
+            <FormAddService />
+            <div className=" bg-black border lg:rotate-90 rotate-180 border-zinc-400"></div>
+            <AddServiceHelp />
+        </div>
     )
 }
 
