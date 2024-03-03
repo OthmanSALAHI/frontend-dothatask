7622d249181a646594d43cf2f2eafc317e2455ab
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-03 16:01:18 +0100

    Fix bug in login functionality

---
+++ src/components/pages/SettingProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SettingProfile() {
+  return (
+    <div>
+        <div className="text-neutral-800 text-5xl font-bold ">My Profile</div>
+    </div>
+  )
+}
+
+export default SettingProfile
