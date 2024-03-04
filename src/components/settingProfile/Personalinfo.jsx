be863207114669ed2342858ea7b762d0e34f19d4
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:59:45 +0100

    Update input field widths in Personalinfo component

--- src/components/settingProfile/Personalinfo.jsx
+++ src/components/settingProfile/Personalinfo.jsx
@@ -24,11 +24,11 @@ function Personalinfo() {
         </div>
         <div className='flex flex-col gap-2.5 my-2'>
           <div className="text-neutral-600 text-base font-normal">Address</div>
-          <input type='text' value='123 Main Street, Anchorage, USA 12345' className="md:w-[490px] w-[130px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          <input type='text' value='123 Main Street, Anchorage, USA 12345' className="md:w-[490px] w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
         </div>
         <div className='flex flex-col gap-2.5 my-2'>
           <div className="text-neutral-600 text-base font-normal">City</div>
-          <input type='text' value='Dakhla' className="md:w-[490px] w-[130px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          <input type='text' value='Dakhla' className="md:w-[490px] w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
         </div>
         <button type='submit' className="p-2.5 mt-2 bg-do_green hover:bg-do_hover_green rounded-[5px] justify-end items-center gap-2.5">
           <div className="text-white text-base font-medium ">Save changes</div>
