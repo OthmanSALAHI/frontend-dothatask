57166b1ef56ccdfd5a7ec7f4bbb5ab3beb0b8eae
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:54:49 +0100

    Update margin in SettingProfile component

--- src/components/pages/SettingProfile.jsx
+++ src/components/pages/SettingProfile.jsx
@@ -4,7 +4,7 @@ import Personaldescription from '../settingProfile/Personaldescription'
 
 function SettingProfile() {
   return (
-    <div className='m-32'>
+    <div className='lg:m-32 m-0'>
         <div className='flex flex-col gap-16'>
             <div className="text-neutral-800 text-5xl font-bold ">My Profile</div>
             <div className='flex gap-10 mx-auto'>
