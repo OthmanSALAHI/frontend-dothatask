d1e8bcb7decc6a3b90778244e1c0631359507d06
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:49:17 +0100

    Update layout in SettingProfile component

--- src/components/pages/SettingProfile.jsx
+++ src/components/pages/SettingProfile.jsx
@@ -7,7 +7,7 @@ function SettingProfile() {
     <div className='m-32'>
         <div className='flex flex-col gap-16'>
             <div className="text-neutral-800 text-5xl font-bold ">My Profile</div>
-            <div className='flex gap-10'>
+            <div className='flex gap-10 mx-auto'>
               <Personalinfo />
               <Personaldescription />
             </div>
