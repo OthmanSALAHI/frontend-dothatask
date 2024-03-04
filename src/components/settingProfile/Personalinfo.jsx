f09597953e9b1bc5a129a8a30d335d21d42bc914
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:56:56 +0100

    Update Personalinfo component styles

--- src/components/settingProfile/Personalinfo.jsx
+++ src/components/settingProfile/Personalinfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Personalinfo() {
   return (
-    <div className="md:w-[650px] w-full h-[538px] p-14 pb-4 bg-white shadow flex-col justify-start items-start gap-10">
+    <div className="md:w-[650px] w-full md:h-[538px] h-auto p-14 pb-4 bg-white shadow flex-col justify-start items-start gap-10">
         <div className="text-neutral-800 text-2xl font-medium mb-10">Personal Information</div>
         <div className='grid md:grid-cols-2 grid-cols-1 gap-0 my-2'>
           <div className="md:w-[291px] w-[130px] h-[76px] flex-col justify-start items-start gap-2.5">
@@ -24,11 +24,11 @@ function Personalinfo() {
         </div>
         <div className='flex flex-col gap-2.5 my-2'>
           <div className="text-neutral-600 text-base font-normal">Address</div>
-          <input type='text' value='123 Main Street, Anchorage, USA 12345' className="w-[490px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          <input type='text' value='123 Main Street, Anchorage, USA 12345' className="md:w-[490px] w-[130px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
         </div>
         <div className='flex flex-col gap-2.5 my-2'>
           <div className="text-neutral-600 text-base font-normal">City</div>
-          <input type='text' value='Dakhla' className="w-[490px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          <input type='text' value='Dakhla' className="md:w-[490px] w-[130px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
         </div>
         <button type='submit' className="p-2.5 mt-2 bg-do_green hover:bg-do_hover_green rounded-[5px] justify-end items-center gap-2.5">
           <div className="text-white text-base font-medium ">Save changes</div>
