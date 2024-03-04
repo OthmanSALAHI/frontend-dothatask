810a7fea6800e79e0a7a32de42ea3f05761745cf
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:48:23 +0100

    Add Personaldescription component to SettingProfile page

--- src/components/pages/SettingProfile.jsx
+++ src/components/pages/SettingProfile.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Personalinfo from '../settingProfile/Personalinfo'
+import Personaldescription from '../settingProfile/Personaldescription'
 
 function SettingProfile() {
   return (
     <div className='m-32'>
         <div className='flex flex-col gap-16'>
             <div className="text-neutral-800 text-5xl font-bold ">My Profile</div>
-            <Personalinfo />
+            <div className='flex gap-10'>
+              <Personalinfo />
+              <Personaldescription />
+            </div>
         </div>
     </div>
   )
