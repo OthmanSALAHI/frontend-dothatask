e9a071d86c327d76fd027f2bb18efc8034f1debd
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:36:37 +0100

    Refactor Personalinfo component layout and input fields

--- src/components/settingProfile/Personalinfo.jsx
+++ src/components/settingProfile/Personalinfo.jsx
@@ -4,10 +4,35 @@ function Personalinfo() {
   return (
     <div className="w-[650px] h-[538px] p-14 bg-white shadow flex-col justify-start items-start gap-10">
         <div className="text-neutral-800 text-2xl font-medium mb-10">Personal Information</div>
-        <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5 inline-flex">
-            <div className="text-neutral-600 text-base font-normal ">First name</div>
-            <input type='text' value='Alexender' className="w-[291px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+        <div className='grid grid-cols-2 gap-0 my-2'>
+          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+              <div className="text-neutral-600 text-base font-normal ">First name</div>
+              <input type='text' value='Alexender' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          </div>
+          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+              <div className="text-neutral-600 text-base font-normal ">Last name</div>
+              <input type='text' value='Eljah' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          </div>
+          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+              <div className="text-neutral-600 text-base font-normal ">birth date</div>
+              <input type='text' value='12/01/2000' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          </div>
+          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+              <div className="text-neutral-600 text-base font-normal ">phone number</div>
+              <input type='text' value='Alexender' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+          </div>
         </div>
+        <div className='flex flex-col gap-2.5 my-2'>
+          <div className="text-neutral-600 text-base font-normal">Address</div>
+          <input type='text' value='123 Main Street, Anchorage, USA 12345' className="w-[490px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+        </div>
+        <div className='flex flex-col gap-2.5 my-2'>
+          <div className="text-neutral-600 text-base font-normal">City</div>
+          <input type='text' value='Dakhla' className="w-[490px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
+        </div>
+        <button type='submit' className="w-[119px] h-[38px] my-5 p-2.5 bg-do_green hover:bg-do_hover_green rounded-[5px] justify-center items-center gap-2.5 inline-flex">
+          <div className="text-white text-base font-medium ">Save changes</div>
+      </button>
     </div>
   )
 }
