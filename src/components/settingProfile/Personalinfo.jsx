43dd0138bf230cedd51e1ae9d32fe8019e0a6720
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 19:54:54 +0100

    Update Personalinfo component styles

--- src/components/settingProfile/Personalinfo.jsx
+++ src/components/settingProfile/Personalinfo.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 function Personalinfo() {
   return (
-    <div className="w-[650px] h-[538px] p-14 pb-4 bg-white shadow flex-col justify-start items-start gap-10">
+    <div className="md:w-[650px] w-full h-[538px] p-14 pb-4 bg-white shadow flex-col justify-start items-start gap-10">
         <div className="text-neutral-800 text-2xl font-medium mb-10">Personal Information</div>
-        <div className='grid grid-cols-2 gap-0 my-2'>
-          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+        <div className='grid md:grid-cols-2 grid-cols-1 gap-0 my-2'>
+          <div className="md:w-[291px] w-[130px] h-[76px] flex-col justify-start items-start gap-2.5">
               <div className="text-neutral-600 text-base font-normal ">First name</div>
               <input type='text' value='Alexender' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
           </div>
-          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+          <div className="md:w-[291px] w-[130px] h-[76px] flex-col justify-start items-start gap-2.5">
               <div className="text-neutral-600 text-base font-normal ">Last name</div>
               <input type='text' value='Eljah' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
           </div>
-          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+          <div className="md:w-[291px] w-[130px] h-[76px] flex-col justify-start items-start gap-2.5">
               <div className="text-neutral-600 text-base font-normal ">birth date</div>
               <input type='text' value='12/01/2000' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
           </div>
-          <div className="w-[291px] h-[76px] flex-col justify-start items-start gap-2.5">
+          <div className="md:w-[291px] w-[130px] h-[76px] flex-col justify-start items-start gap-2.5">
               <div className="text-neutral-600 text-base font-normal ">phone number</div>
               <input type='text' value='Alexender' className="w-[225px] h-12 p-2.5 bg-white rounded-[5px] shadow justify-start items-center gap-[113px] inline-flex" />
           </div>
