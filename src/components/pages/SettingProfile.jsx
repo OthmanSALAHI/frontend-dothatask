815bc5c13ab5c57b8c1771e9d6f8dd1ec9c9f2df
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-04 20:11:32 +0100

    Update My Profile heading style

--- src/components/pages/SettingProfile.jsx
+++ src/components/pages/SettingProfile.jsx
@@ -6,7 +6,7 @@ function SettingProfile() {
   return (
     <div className='lg:m-32 m-0'>
         <div className='flex flex-col gap-16'>
-            <div className="text-neutral-800 text-5xl font-bold ">My Profile</div>
+            <div className="text-neutral-800 lg:text-5xl md:text-3xl text-2xl font-bold mx-4">My Profile</div>
             <div className='flex lg:flex-row flex-col gap-14 mx-auto'>
               <Personalinfo />
               <Personaldescription />
