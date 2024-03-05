2e46971e6eb01146144d73528a07ec3463037b58
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-05 20:30:01 +0100

    Update font size and class names in TypePage component

--- src/components/Auth/TypePage.jsx
+++ src/components/Auth/TypePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function TypePage() {
   return (
-    <div className='mx-auto my-8'>
-        <div className="w-auto text-center text-stone-950 text-5xl font-bold font-['Nunito Sans']">Register as a client or freelancer</div>
+    <div className='mx-auto my-24'>
+        <div className="w-auto text-center text-stone-950 text-4xl font-bold font-nunito">Register as a client or freelancer</div>
     </div>
   )
 }
