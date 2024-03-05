cff2f5c564c269466e6d403544e89980ec0f4ce0
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-03-05 20:17:55 +0100

    Add TypePage component

---
+++ src/components/Auth/TypePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TypePage() {
+  return (
+    <div>
+        <div className="w-[736px] text-center text-stone-950 text-5xl font-bold font-['Nunito Sans']">Register as a client or freelancer</div>
+    </div>
+  )
+}
+
+export default TypePage
